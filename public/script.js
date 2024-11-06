0e28ff99fc59b401253e816f7dca1fb3b4c96f62
AlumG <AlumG@majada.es>
2024-11-06 20:26:07 +0100

    deploy

--- public/script.js
+++ public/script.js
@@ -29,7 +29,7 @@ class header extends HTMLElement {
                     </a>
                     </div>
                 <div class="titulo">
-                    <h4>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h4>
+                    <h4>POKEMON</h4>
                 </div>
             </header >
         `;
